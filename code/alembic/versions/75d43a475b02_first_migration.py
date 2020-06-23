162271ccd1e51f47a8a740dee3664efb7b64ebdb
PLsergent <papa.louis59@gmail.com>
2020-06-23 18:00:54 +0200

    Init fastAPI with Postgresql + alembic

---
+++ code/alembic/versions/75d43a475b02_first_migration.py
@@ -0,0 +1,24 @@
+"""First migration
+
+Revision ID: 75d43a475b02
+Revises: 
+Create Date: 2020-06-23 15:17:55.021970
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '75d43a475b02'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
