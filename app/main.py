77323593707dc1cd812f7fbe9857dbe990608242
PLsergent <papa.louis59@gmail.com>
2020-06-24 23:07:52 +0200

    Add Cors

--- app/main.py
+++ app/main.py
@@ -11,9 +11,16 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000"
+]
+
 app.include_router(api_router)
 app.add_middleware(
         CORSMiddleware,
+        allow_origins=origins,
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
