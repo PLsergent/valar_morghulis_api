2472083c54a8c2782b15e022b93df32e65591dc5
PLsergent <papa.louis59@gmail.com>
2020-06-24 17:14:03 +0200

    Avoid creating postgres extension when not needed

--- scripts/initdb/create_extensions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-create extension if not exists "uuid-ossp";
-EOSQL
